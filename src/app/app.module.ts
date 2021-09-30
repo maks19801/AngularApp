e389ee87c193e3c2599aa308caa4d42c8078be21
Порховский Максим <maks19801@gmail.com>
2021-09-30 09:16:33 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+
+@Component({
+  selector: 'app-user',
+  templateUrl:'./user.component.html',
+  styleUrls: ['./user.component.css'],
+  //template: '<h2>John Doe</h2>'
+  // styles:[`
+  //   h2 {
+  //     color: blue
+  //   }
+  // `]
+})
+
+export class UserComponent implements OnInit {
+  //Properties
+  user!: User;
+  //Methods
+  constructor() {
+
+  }
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com'
+    }
+  }
+}
+
+
